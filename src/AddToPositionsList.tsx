1ca0b063d3658aec44c65a06785d76707d55a773
Ian Boyle <irboyle21@gmail.com>
2022-01-29 15:17:51 -0500

    adds AddToPositionList component

--- src/AddToPositionsList.tsx
+++ src/AddToPositionsList.tsx
@@ -1,12 +1,53 @@
-import React from "react"
-
+import React, {ChangeEvent, useState} from "react"
+import axios from "axios"
+import {Position as Props} from "./YahooAPI"
 // interface IProps {
 //   onChange:  
 // }
-
-const AddToPositionsList = () => {
+interface IProps {
+  positions: Props["positions"]
+  setPositions: React.Dispatch<React.SetStateAction<Props["positions"]>>
+}
+const AddToPositionsList: React.FC<IProps> = ({positions, setPositions}) => {
+  const [newPosition, setNewPosition] = useState("")
+  const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
+    setNewPosition(event.target.value)
+  }
+  const handleClick = ():void => {
+    var options: object = {
+      method: 'GET',
+      url: `https://yfapi.net/v11/finance/quoteSummary/${newPosition}`,
+      params: {modules: 'defaultKeyStatistics,assetProfile'},
+      headers: {
+        'x-api-key': `${process.env.REACT_APP_YAHOO_API_KEY}`
+      }
+    };
+    
+     axios.request(options).then(function (response) {
+      console.log(response.data.quoteSummary.result[0]);
+      if(!response.data.quoteSummary.result){return}
+      setPositions([
+        ...positions,
+        {
+          name: newPosition,
+          sector: response.data.quoteSummary.result[0].assetProfile.sector,
+          country: response.data.quoteSummary.result[0].assetProfile.country,
+          state: response.data.quoteSummary.result[0].assetProfile.state,
+          longBusinessSummary: response.data.quoteSummary.result[0].assetProfile.longBusinessSummary
+        }
+      ])
+      console.log(positions)
+    }).catch(function (error) {
+      console.error(error);
+    });
+           
+  }
   return (
-    <div><input type="text" placeholder="symbol"/></div>
+    <div>
+    <input onChange={handleChange} value={newPosition} type="text" placeholder="symbol"/>
+    <button onClick={handleClick}>Submit</button>
+    </div>
+
   )
 }
 
--- src/YahooAPI.tsx
+++ src/YahooAPI.tsx
@@ -1,5 +1,4 @@
-import React, {useState, ChangeEvent, useEffect} from 'react';
-import axios from "axios"
+import React, {useState} from 'react';
 import PositionsList from './PositionsList';
 import AddToPositionsList from './AddToPositionsList';
 
@@ -21,54 +20,12 @@ const Yahoo = () => {
     state: "CA",
     longBusinessSummary: "feouwhfew"
   }])
-  const [newPosition, setNewPosition] = useState("")
-  const handleChange = (event: ChangeEvent<HTMLInputElement>):void => {
-    setNewPosition(event.target.value)
-  }
-  
-  const getPositionInfo = async () => {
-    var options: object = {
-      method: 'GET',
-      url: `https://yfapi.net/v11/finance/quoteSummary/${newPosition}`,
-      params: {modules: 'defaultKeyStatistics,assetProfile'},
-      headers: {
-        'x-api-key': `${process.env.REACT_APP_YAHOO_API_KEY}`
-      }
-    };
-    
-    await axios.request(options).then(function (response) {
-      console.log(response.data.quoteSummary.result[0]);
-      // setPositions(response.data.quoteSummary.result[0].assetProfile);
-      setPositions([
-        ...positions,
-        {
-          name: newPosition,
-          sector: response.data.quoteSummary.result[0].assetProfile.sector,
-          country: response.data.quoteSummary.result[0].assetProfile.country,
-          state: response.data.quoteSummary.result[0].assetProfile.state,
-          longBusinessSummary: response.data.quoteSummary.result[0].assetProfile.longBusinessSummary
-        }
-      ])
-      // setNewPosition({
-      //   name: event.target.value,
-      //   sector: response.data.quoteSummary.result[0].assetProfile.sector,
-      //   country: response.data.quoteSummary.result[0].assetProfile.country,
-      //   state: response.data.quoteSummary.result[0].assetProfile.state,
-      //   longBusinessSummary: response.data.quoteSummary.result[0].assetProfile.longBusinessSummary,
-      // })
-      console.log(positions)
-    }).catch(function (error) {
-      console.error(error);
-    });
-           
-  }
+
   return (
   <div>
-    <input onChange={handleChange} value={newPosition} type="text" placeholder="symbol"/>
-    <button onClick={getPositionInfo}>Submit</button>
-    {/* {positions?.map((p) => {return p.sector})} */}
+    <AddToPositionsList positions={positions} setPositions={setPositions}/>
     <PositionsList positions={positions}/>
-    <AddToPositionsList />
+   
   </div> )
 }
 
